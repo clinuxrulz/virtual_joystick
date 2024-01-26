23bdccc7c38aba86fdb40176fa0f6107c6c0c272
Clinton Selke <clinuxrulz@gmail.com>
2024-01-26 11:42:51 +1000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -52,11 +52,6 @@ impl<S: VirtualJoystickID> Plugin for VirtualJoystickPlugin<S> {
                 update_joystick
                     .before(update_input::<S>),
             )
-            .add_systems(
-                PreUpdate,
-                update_joystick_by_mouse
-                    .before(update_input::<S>),
-            )
             .add_systems(PreUpdate, update_input::<S>)
             .add_systems(
                 PostUpdate,
